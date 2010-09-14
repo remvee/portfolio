dfcdfb4d0498332577e8cfb70d3ccb13d61077bf
Remco van 't Veer <rwvtveer@xs4all.nl>
2010-09-14 11:58:07 +0200

    removed unused dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,5 @@
                  [compojure "0.4.1"]
                  [hiccup "0.2.6"]
                  [ring/ring-jetty-adapter "0.2.5"]
-                 [ring/ring-devel "0.2.5"]
-                 [ring-basic-authentication "0.0.0-SNAPSHOT"]
-                 [congomongo "0.1.3-SNAPSHOT"]]
+                 [ring/ring-devel "0.2.5"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
