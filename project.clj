bbd3091029b4605ff0ba0d79e262b3f3b5c9ba4c
Remco van 't Veer <rwvtveer@xs4all.nl>
2010-09-21 10:29:25 +0200

    upgrade ring

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.4.1"]
                  [hiccup "0.2.6"]
-                 [ring/ring-jetty-adapter "0.2.5"]
-                 [ring/ring-devel "0.2.5"]]
+                 [ring/ring-jetty-adapter "0.3.0"]
+                 [ring/ring-devel "0.3.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [lein-run "1.0.0"]]
   :run-aliases {:server ["script/server.clj"]})
