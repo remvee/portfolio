3221171fb640167a02f8712912a3e8318e2cc035
Remco van 't Veer <rwvtveer@xs4all.nl>
2010-10-01 15:56:28 +0200

    upgrade basic-authentication middleware

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [hiccup "0.2.6"]
                  [ring/ring-jetty-adapter "0.3.0"]
                  [ring/ring-devel "0.3.0"]
-                 [ring-basic-authentication "0.0.0-SNAPSHOT"]]
+                 [ring-basic-authentication "0.0.1-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [lein-run "1.0.0"]]
   :run-aliases {:server ["script/server.clj"]})
